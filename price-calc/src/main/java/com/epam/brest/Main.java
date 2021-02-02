30d621ccca5757b9711c1894d2c1fa153f08457f
Denis Milashuk <ilovehardrock@mail.ru>
2021-02-02 20:35:19 +0300

    Added Main.class

---
+++ price-calc/src/main/java/com/epam/brest/Main.java
@@ -0,0 +1,17 @@
+package com.epam.brest;
+
+public class Main {
+    public static void main(String[] args) {
+        // Distance
+        // Price par km
+        // Weight
+        // Price per kg
+        // Result = d * pr1 + w * pr2
+        double d = 10;
+        double pr1 = 20.05;
+        double w = 30;
+        double pr2 = 30.5;
+        double result = d * pr1 + w * pr2;
+        System.out.println(result);
+    }
+}

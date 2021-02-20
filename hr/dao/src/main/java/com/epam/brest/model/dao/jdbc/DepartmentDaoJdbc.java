b46927714be08efd1ba01c8b98805076b4c86e67
Denis Milashuk <ilovehardrock@mail.ru>
2021-02-20 17:56:50 +0300

    first part hr app

---
+++ hr/dao/src/main/java/com/epam/brest/model/dao/jdbc/DepartmentDaoJdbc.java
@@ -0,0 +1,12 @@
+package com.epam.brest.model.dao.jdbc;
+
+import com.epam.brest.Department;
+import com.epam.brest.model.dao.DepartmentDao;
+
+import java.util.List;
+
+public class DepartmentDaoJdbc implements DepartmentDao {
+    public List<Department> findAll() {
+        return null;
+    }
+}
